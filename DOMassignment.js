99fe8bcf930b8c299acb84242893e1fcd946d4d2
Mac Taylor <Mac@Macs-MacBook-Air.local>
2017-04-11 14:23:20 -0400

    commit often

--- DOMassignment.js
+++ DOMassignment.js
@@ -1,1 +1,17 @@
 // JavaScript for DOMassignment.html
+
+let profiles = [
+    { name: , picture: ,},
+    { name: , picture: ,},
+    { name: , picture: ,},
+];
+
+function init() {
+    
+    console.log('this is working');
+
+
+
+}
+
+window.addEventListener('load', init);
